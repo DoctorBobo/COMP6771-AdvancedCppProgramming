af2f1152db77aa440f09db38483e38e2152fb8da
Christopher Di Bella <cjdb.ns@gmail.com>
2016-11-08 01:35:07 +1100

    Copy semantics unit tests

---
+++ exam-prep/test2a.cpp
@@ -0,0 +1,25 @@
+#include "Bad_list.hpp"
+#include <algorithm>
+#include <cassert>
+
+int main()
+{
+   constexpr auto value = 20;
+   auto a = Bad_list<decltype(value)>(100, value);
+   auto b = a;
+   assert(a == b);
+   assert(!(a != b));
+   assert(a.front() == value);
+   assert(a.front() == b.front());
+   assert(std::addressof(a.front()) != std::addressof(b.front()));
+   assert(a.back() == value);
+   assert(std::addressof(a.back()) != std::addressof(b.back()));
+   assert(a.begin() != b.begin());
+   assert(std::addressof(*a.begin()) != std::addressof(*b.begin()));
+   assert(*a.begin() == *b.begin());
+   assert(a.end() != b.end());
+   assert(a.rbegin() != b.rbegin());
+   assert(std::addressof(*a.rbegin()) != std::addressof(*b.rbegin()));
+   assert(*a.rbegin() == *b.rbegin());
+   assert(a.rend() != b.rend());
+}
