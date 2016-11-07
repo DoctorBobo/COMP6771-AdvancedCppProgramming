5b84350ddf9f2a637dbc7d8968e48bb9dffeaa57
Christopher Di Bella <cjdb.ns@gmail.com>
2016-11-08 01:39:44 +1100

    Testing iterator and range constructors

---
+++ exam-prep/test1c.cpp
@@ -0,0 +1,22 @@
+#include "Bad_list.hpp"
+#include <cassert>
+#include <memory>
+#include <numeric>
+#include <vector>
+
+template <typename T>
+using value_type_t = typename T::value_type;
+
+template <typename T>
+using difference_type_t = typename T::difference_type;
+
+int main()
+{
+   const auto v = std::vector<int>{0, 1, 2, 3, 4, 5};
+   const auto l = Bad_list<value_type_t<decltype(v)>>{v};
+
+   assert(static_cast<difference_type_t<decltype(l)>>(v.size()) == l.size());
+   assert(std::equal(v.begin(), v.end(), l.begin(), l.end()));
+   assert(std::addressof(*v.begin()) != std::addressof(*l.begin()));
+   assert(std::accumulate(l.begin(), l.end(), 0U) == (v.size() / 2) * (v.back() + v.front()));
+}
