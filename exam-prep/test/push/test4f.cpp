7a4ba696c11598398cdcdc2f302ba159569518ce
Christopher Di Bella <cjdb.ns@gmail.com>
2016-11-08 18:58:04 +1100

    Moved push_front/push_back tests into their own subdirectory

--- exam-prep/test/push/test4f.cpp
+++ exam-prep/test/push/test4f.cpp
@@ -1,16 +1,16 @@
 #include "Bad_list.hpp"
 #include <algorithm>
 #include <cassert>
-#include "Copyable.hpp"
+#include "copyable.hpp"
 
 int main()
 {
    auto a = Bad_list<Copyable>{};
-   a.push_front(Copyable());
+   a.push_back(Copyable());
    assert(a.size() == 1);
    assert(Copyable::default_ctor() == 3);
    assert(Copyable::copy_ctor() == 0);
    assert(Copyable::copy_assign() == 0);
-   assert(Copyable::move_ctor() == 6);
+   assert(Copyable::move_ctor() == 7);
    assert(Copyable::move_assign() == 0);
 }
