1ab40a5b0aa25e3c47e1355a81e2496f33912026
Christopher Di Bella <cjdb.ns@gmail.com>
2016-11-08 14:06:49 +1100

    Modified copy push_back/push_front tests so that they are better regulated

--- exam-prep/test4c.cpp
+++ exam-prep/test4c.cpp
@@ -1,14 +1,21 @@
 #include "Bad_list.hpp"
 #include <algorithm>
 #include <cassert>
+#include <deque>
+#include "types.hpp"
 
 int main()
 {
-   auto a = Bad_list<int>{};
-   constexpr auto size = 100;
-   for (auto i = 0; i < size; ++i)
-      a.push_front(i);
-   assert(a.size() == size);
-   for (auto i = 0; i < size; ++i, a.pop_back())
-      assert(i == a.back());
+   auto d = std::deque<int>{};
+   for (auto i = 0; i < 100; ++i)
+      d.push_front(i);
+   auto l = Bad_list<int>{};
+   for (auto i : d) {
+      l.push_back(i);
+      assert(l.front() == d.front());
+   }
+
+   assert(l.size() == difference_type_t<decltype(l)>(d.size()));
+   for (; !l.empty(); d.pop_front(), l.pop_front())
+      assert(l.front() == d.front());
 }
--- exam-prep/test4d.cpp
+++ exam-prep/test4d.cpp
@@ -1,14 +1,21 @@
 #include "Bad_list.hpp"
 #include <algorithm>
 #include <cassert>
+#include <deque>
+#include "types.hpp"
 
 int main()
 {
-   auto a = Bad_list<int>{};
-   constexpr auto size = 100;
-   for (auto i = 0; i < size; ++i)
-      a.push_back(i);
-   assert(a.size() == size);
-   for (auto i = 0; i < size; ++i, a.pop_front())
-      assert(i == a.front());
+   auto d = std::deque<int>{};
+   for (auto i = 0; i < 100; ++i)
+      d.push_back(i);
+   auto l = Bad_list<int>{};
+   for (auto i : d) {
+      l.push_back(i);
+      assert(l.back() == i);
+   }
+
+   assert(l.size() == difference_type_t<decltype(l)>(d.size()));
+   for (; !l.empty(); d.pop_back(), l.pop_back())
+      assert(l.back() == d.back());
 }
